0931dc777a2799d70057d4c911ede72b1093777a
Elizaveta Komarova <lisakomarova20000@gmail.com>
2023-07-26 18:57:46 +0300

    feat: add meals provider

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/data/dummy_meals.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/filter_screen.dart';
 import 'package:meals_app/screens/meals_screen.dart';
@@ -13,14 +14,14 @@ Map<Filter, bool> _kInitialFilters = {
   Filter.vegan: false,
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<TabsScreen> createState() => _TabsScreenState();
+  ConsumerState<TabsScreen> createState() => _TabsScreenState();
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favouriteMeals = [];
   Map<Filter, bool> _selectedFilters = {
@@ -58,8 +59,11 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _setScreen(String screen) async {
     if (screen == 'filters') {
-      final result = await Navigator.of(context).push<Map<Filter, bool>>(
-          MaterialPageRoute(builder: (ctx) => FilterScreen(currentFilters: _selectedFilters,)));
+      final result =
+          await Navigator.of(context).push<Map<Filter, bool>>(MaterialPageRoute(
+              builder: (ctx) => FilterScreen(
+                    currentFilters: _selectedFilters,
+                  )));
       setState(() {
         _selectedFilters = result ?? _kInitialFilters;
       });
@@ -71,7 +75,8 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     String activePageTitle = 'Categories';
-    final availableMeals = dummyMeals.where((meal) {
+    final meals = ref.watch(mealsProvider);
+    final availableMeals = meals.where((meal) {
       if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
         return false;
       }
@@ -86,7 +91,7 @@ class _TabsScreenState extends State<TabsScreen> {
       }
       return true;
     }).toList();
-        Widget activePage = CategoriesScreen(
+    Widget activePage = CategoriesScreen(
       onToggleFavourite: _toggleMealFavouriteStatus,
       availableMeals: availableMeals,
     );
